e809773235ebeea3de5bb5c73166258c02c5a984
Paola Pesantez <p.pesantezcabrera@wsu.edu>
2021-01-26 21:23:16 -0500

    ReadTweets functionality added

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,6 +21,7 @@ func Manejadores() {
 	router.HandleFunc("/verperfil", middlew.ChequearBD(middlew.ValidarJWT(routers.VerPerfil))).Methods("GET")
 	router.HandleFunc("/modificarperfil", middlew.ChequearBD(middlew.ValidarJWT(routers.ModificarPerfil))).Methods("PUT")
 	router.HandleFunc("/tweet", middlew.ChequearBD(middlew.ValidarJWT(routers.CrearTweet))).Methods("POST")
+	router.HandleFunc("/leertweets", middlew.ChequearBD(middlew.ValidarJWT(routers.LeerTweets))).Methods("GET")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
